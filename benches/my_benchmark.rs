366cfd2e2c0ad18253cdc92e9b6f58e41f6f51fe
PvdBerg1998 <PvdBerg1998@users.noreply.github.com>
2022-01-09 18:33:33 +0100

    Add benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,10 +1,18 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use gsl_rust::*;
+use gsl_rust::fft::fft64_packed;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    // c.bench_function("x", |b| {
-    //     b.iter_with_large_drop(|| todo!())
-    // });
+    // Prepare data
+    let y = (0..2u64.pow(20))
+        .map(|x| x as f32 / 2.0f32.powi(18) * std::f32::consts::TAU)
+        .map(|x| x.cos())
+        .collect::<Vec<_>>();
+    c.bench_function("fft64 2^20", |b| {
+        b.iter_with_large_drop(|| {
+            let y = y.clone();
+            fft64_packed(y.as_ref())
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
