805dd6d58178298883e0fd0e9724aaf306a466a2
PvdBerg1998 <PvdBerg1998@users.noreply.github.com>
2022-02-20 20:09:09 +0100

    Remove hurwitz zeta complex

--- src/special.rs
+++ src/special.rs
@@ -19,7 +19,6 @@
 use crate::bindings::*;
 use crate::*;
 use num_complex::Complex64;
-use num_traits::One;
 
 pub fn gamma(x: f64) -> Result<ValWithError<f64>> {
     unsafe {
@@ -63,59 +62,6 @@ pub fn hurwitz_zeta(s: f64, a: f64) -> Result<ValWithError<f64>> {
     }
 }
 
-pub fn hurwitz_zeta_complex(s: Complex64, a: f64) -> Result<ValWithError<Complex64>> {
-    if s == Complex64::one() {
-        return Err(GSLError::Domain);
-    }
-    if a <= 0.0 {
-        return Err(GSLError::Domain);
-    }
-
-    let c_a = Complex64::from(a);
-
-    // NIST 25.11.29. The integral is split into a real and imaginary part.
-
-    let part1 = Complex64::from(0.5) / c_a.powc(s)
-        + c_a.powc(Complex64::one() - s) / (s - Complex64::one());
-
-    let re_integrand = |x: f64| -> f64 {
-        let t = (x / a).atan();
-        let log = (a.powi(2) + x.powi(2)).ln();
-
-        let rez = s.re;
-        let imz = s.im;
-
-        ((0.5 * imz * log).cos() * (imz * t).cosh() * (rez * t).sin()
-            + (rez * t).cos() * (0.5 * imz * log).sin() * (imz * t).sinh())
-            / (((std::f64::consts::TAU * x).exp() - 1.0)
-                * ((a.powi(2) + x.powi(2)).powf(0.5 * rez)))
-    };
-
-    let im_integrand = |x: f64| -> f64 {
-        let t = (x / a).atan();
-        let log = (a.powi(2) + x.powi(2)).ln();
-
-        let rez = s.re;
-        let imz = s.im;
-
-        (-(0.5 * imz * log).sin() * (imz * t).cosh() * (rez * t).sin()
-            + (rez * t).cos() * (0.5 * imz * log).cos() * (imz * t).sinh())
-            / (((std::f64::consts::TAU * x).exp() - 1.0)
-                * ((a.powi(2) + x.powi(2)).powf(0.5 * rez)))
-    };
-
-    let re_part2 = integration::qagiu(0.0, re_integrand)?;
-    let im_part2 = integration::qagiu(0.0, im_integrand)?;
-
-    let part2 = Complex64::new(2.0 * re_part2.val, 2.0 * im_part2.val);
-    let err = Complex64::new(2.0 * re_part2.err, 2.0 * im_part2.err);
-
-    Ok(ValWithError {
-        val: part1 + part2,
-        err,
-    })
-}
-
 #[test]
 fn test_gamma() {
     disable_error_handler();
@@ -136,35 +82,4 @@ fn test_gamma() {
         -0.1549498283018,
         epsilon = 1.0e-9
     );
-}
-
-#[test]
-fn test_hurwitz_zeta_compare_rs_gsl() {
-    disable_error_handler();
-
-    for i in 1..100 {
-        let s = i as f64 + 0.1;
-
-        for a in 1..10 {
-            let a = a as f64;
-
-            let rust_zeta = hurwitz_zeta_complex(Complex64::from(s), a).unwrap().val.re;
-            let gsl_zeta = hurwitz_zeta(s, a).unwrap().val;
-
-            approx::assert_abs_diff_eq!(rust_zeta, gsl_zeta, epsilon = 1.0e-9);
-        }
-    }
-}
-
-#[test]
-fn test_hurwitz_zeta() {
-    disable_error_handler();
-
-    let z = hurwitz_zeta_complex(Complex64::new(5.0, 5.0), 1.5)
-        .unwrap()
-        .val;
-
-    // Impressive accuracy of the approximation!
-    approx::assert_abs_diff_eq!(z.re, -0.057538922474198148085);
-    approx::assert_abs_diff_eq!(z.im, -0.108623432041536612067)
 }
